e171e4d7559c00593a4a87cce0eb26ae971fe095
Subiksha <68904212+subhikshapannirselvam@users.noreply.github.com>
2022-11-27 10:10:28 +0530

    Add files via upload

---
+++ Java/Multithreading.java
@@ -0,0 +1,25 @@
+class Multithread extends Thread {
+    public void run()
+    {
+        try {
+            System.out.println(
+                "Thread " + Thread.currentThread().getId()+ " is running");
+        }
+        catch (Exception e) {
+          
+            System.out.println("Exception is caught");
+        }
+    }
+}
+ 
+
+public class Multithreading {
+    public static void main(String[] args)
+    {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            Multithread obj= new Multithread();
+            obj.start();
+        }
+    }
+}
